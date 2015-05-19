8b42aba8112f6ad35fdfe6bb03c631494e8861f2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2015-05-20 07:45:33 +0900

    call initgroups

--- setusergroups.c
+++ setusergroups.c
@@ -7,73 +7,33 @@
 #include <pwd.h>
 #include <grp.h>
 
-static void set_supp_groups(const char* username) {
-  int ngroups=0;
-  int capacity=100;
-  gid_t* gidset = malloc(sizeof(gid_t)*capacity);
-  if (gidset == NULL) {
-      printf("Out of memory\n");
-      exit(1);
-  }
-
-  while (1) {
-    struct group* grent = getgrent();
-    if (grent == NULL) {
-      break;
-    }
-
-    char **gr_mem = grent->gr_mem;
-    while (*gr_mem) {
-      if (strcmp(*gr_mem, username) == 0) {
-        if (ngroups+1 > capacity) {
-          capacity = capacity * 2;
-
-          gidset = realloc(gidset, sizeof(gid_t)*capacity);
-          if (gidset == NULL) {
-              printf("Out of memory\n");
-              exit(1);
-          }
-        }
-
-        gidset[ngroups++] = grent->gr_gid;
-        break;
-      }
-      gr_mem++;
-    }
-  }
-
-  if (setgroups(ngroups, gidset) != 0) {
-    perror("setgroups failed");
-    exit(1);
-  }
-
-  free(gidset);
-}
-
 int main(int argc, char** argv) {
   if (argc <= 2) {
     printf("Usage: %s account child\n", argv[0]);
-    return 1;
+    return 111;
   }
 
-  char* name = argv[1];
+  char* account = argv[1];
 
-  struct passwd *pwnam = getpwnam(name);
+  struct passwd *pwnam = getpwnam(account);
   if (pwnam == NULL) {
     perror("unknown user");
-    return 1;
+    return 111;
   }
 
   if (setgid(pwnam->pw_gid) != 0) {
     perror("setgid failed");
-    return 1;
+    return 111;
   }
 
-  set_supp_groups(name);
+  if (initgroups(account, pwnam->pw_gid) != 0) {
+    perror("initgroups failed");
+    return 111;
+  }
 
   if (setuid(pwnam->pw_uid) != 0) {
     perror("setuid failed");
-    return 1;
+    return 111;
   }
 
   execvp(argv[2], &(argv[2]));
